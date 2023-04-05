c7eaf90f00881a8653d58e662aff99b99f31f7e8
Mariano Rivero <marianorivero97@gmail.com>
2023-04-05 14:35:44 -0300

    Se agrego un boton hacia welcome.blade.php desde index

--- mariano-app/resources/views/index.blade.php
+++ mariano-app/resources/views/index.blade.php
@@ -5,10 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        a:link{
+	        border: 1px solid #2075d5;
+	        padding: 15px 25px;
+	        text-align: center;
+	        text-decoration: none;
+	        display: inline-block;
+        }
+
+        a:hover{
+	    background-color #:2075d5;
+	    color: #ffffff;
+        }
+    </style>
+
+
 </head>
 <body>
     <!--Crear un boton que redireccione a "welcome"-->
-
+    
+    <a href="welcome.blade.php"> Boton </a>
 
 </body>
 </html>
