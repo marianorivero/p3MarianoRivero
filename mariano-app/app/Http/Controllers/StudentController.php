841d708e0a9f15fedcf7680455f8d6bdf168bb5f
Mariano Rivero <marianorivero97@gmail.com>
2023-05-03 18:20:24 -0300

    03-05-2023

--- mariano-app/app/Http/Controllers/StudentController.php
+++ mariano-app/app/Http/Controllers/StudentController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+//agrego ruta hacia el Modelo
 use App\Models\Student;
 
 class StudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
 		$students = Student::all();
@@ -17,49 +17,37 @@ class StudentController extends Controller
        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(string $id)
     {
         dd($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(string $id)
     {
         //
--- mariano-app/routes/web.php
+++ mariano-app/routes/web.php
@@ -1,22 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//agrego ruta hacia el controlador
 use App\Http\Controllers\StudentController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/create', [StudentController::class, 'create'])->name(StudentController.create)
 
 Route::resource('students', StudentController::class);
 
