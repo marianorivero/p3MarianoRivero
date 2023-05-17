64002022b46b22b84efc5d97fb5d2205b002a3a4
Mariano Rivero <marianorivero97@gmail.com>
2023-05-17 16:41:15 -0300

    17-05-2023

--- mariano-app/resources/views/dashboard.blade.php
+++ mariano-app/resources/views/dashboard.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __("Estas loggeado!") }}
+                    <br><br>
+                    <a href="/students"><button>Alumnos</button></a><br><hr><br>
+                    <a href="/careers"><button>Carreras</button></a><br><hr><br>
+                    <a href="/subjects"><button>Materias</button></a><br><hr>
                 </div>
             </div>
         </div>
